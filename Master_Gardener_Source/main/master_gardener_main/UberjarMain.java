bd725fe45d1586be3af0dcb2c549be6ed06b5a89
Logan Harris <lharri11@ycp.edu>
2018-03-15 21:12:29 -0400

    Initial website commit

--- Master_Gardener_Source/main/master_gardener_main/UberjarMain.java
+++
@@ -1,9 +0,0 @@
-package master_gardener_main;
-
-// Launch from an uberjar.
-public class UberjarMain {
-	public static void main(String[] args) throws Exception {
-		UberjarDaemonController controller = new UberjarDaemonController();
-		controller.exec(args);
-	}
-}
